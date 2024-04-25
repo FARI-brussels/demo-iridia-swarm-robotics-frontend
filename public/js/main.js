4b46c49bfbcf5b2812bb4bddddad9bb40f42c98a
m0satron <18460264+m0satron@users.noreply.github.com>
2024-04-25 12:11:17 +0200

    feat: update models and render script

--- public/js/main.js
+++ public/js/main.js
@@ -1,44 +1,74 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import {loadGLB, loadSTL, loadOBJ} from './loaders.js'
-
+import { loadGLB } from './loaders.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 const drones = []; // Array to hold the drones
 const scene = new THREE.Scene();
-scene.background = new THREE.Color("#0C95E4");
 
 // Number of drones you want to load
 const N = 5; // For example, loading 5 drones
 // Load N drones
 for (let i = 0; i < N; i++) {
-  const drone = loadGLB(scene, "./assets/drone.glb", null,  
-  {
-    scale:{x: 0.02, y: 0.02, z: 0.02}, 
-    position:{x:1000, y:1000, z:20}, 
-    rotation: { x: Math.PI / 2, y: 0, z: 0 },
-  },
-  (loadedDrone) => { // This callback function is executed after the drone is loaded
-    drones.push(loadedDrone); // Append the loaded drone to the drones array
-  }
+  const drone = loadGLB(scene, "./assets/drone.glb", null,
+    {
+      scale: { x: 0.02, y: 0.02, z: 0.02 },
+      position: { x: 1000, y: 1000, z: 20 },
+      rotation: { x: Math.PI / 2, y: 0, z: 0 },
+    },
+    (loadedDrone) => { // This callback function is executed after the drone is loaded
+      drones.push(loadedDrone); // Append the loaded drone to the drones array
+    }
   );
 }
 
-// Ground plane
-const groundMaterial = new THREE.MeshBasicMaterial({ color: "#eaeaea" }); // Blue
-const buildingMaterial = new THREE.MeshBasicMaterial({ color: "#808080" }); // Blue
-//const ground = new THREE.Mesh(groundGeometry, groundMaterial);
+//TODO: refactor
+const loader = new GLTFLoader();
+loader.load("./assets/swarm_map.glb", function (gltf) {
+  scene.add(gltf.scene);
+}, undefined, function (error) {
+  console.error('Error loading model:', error);
+});
 
 
-loadSTL(scene, "./assets/tabletop.stl", groundMaterial, {scale: { x: 100, y: 100, z: 100 }})
-loadOBJ(scene, "./assets/buildings.obj", buildingMaterial, {scale: { x: 100, y: 100, z: 100 }, rotation: { x: Math.PI / 2, y: 0, z: 0 }})
+// Set up Camera
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 100;
+const aspectRatio = window.innerWidth / window.innerHeight;
+const frustumHeight = 70;
+const frustumWidth = frustumHeight * aspectRatio;
 
-const renderer = new THREE.WebGLRenderer();
+const camera = new THREE.OrthographicCamera(
+  -frustumWidth / 2, frustumWidth / 2,
+  frustumHeight / 2, -frustumHeight / 2,
+  0.01, 1000
+);
+
+camera.position.set(0, 20, 60);
+
+//Set scene lighting
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 2);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
+directionalLight.position.set(0, 1, 0);
+scene.add(ambientLight);
+scene.add(directionalLight);
+
+
+const renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+
 document.body.appendChild(renderer.domElement);
 
+
+//uncomment for debugging
+// const gridHelper = new THREE.GridHelper(2000, 20);
+// const axesHelper = new THREE.AxesHelper(1000);
+// const cameraHelper = new THREE.CameraHelper(camera);
+
+// scene.add(gridHelper);
+// scene.add(axesHelper);
+// scene.add(cameraHelper);
 
 
 // Create OrbitControls
@@ -46,6 +76,7 @@ const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true; // Optional: this enables inertia for smooth camera movement
 controls.dampingFactor = 0.05;
 controls.screenSpacePanning = false;
+controls.enableZoom = false; // comment out to enable zoom
 
 
 // Function to update drone positions based on the latest message from ROS
@@ -54,8 +85,8 @@ function updateDronePositions(newPositions) {
     // Assuming robot_id matches the drone's index in the drones array
     const drone = drones[position.robot_id];
     if (drone) {
-      drone.position.x = 102- position.position.x/10;
-      drone.position.y = - 40 + position.position.y/10;
+      drone.position.x = 102 - position.position.x / 10;
+      drone.position.y = - 40 + position.position.y / 10;
       drone.position.z = 20// Assuming you want to set z as well
     }
   });
@@ -67,15 +98,15 @@ var ros = new ROSLIB.Ros({
   url: 'ws://localhost:9090' // Replace with your websocket server address
 });
 
-ros.on('connection', function() {
+ros.on('connection', function () {
   console.log('Connected to websocket server.');
 });
 
-ros.on('error', function(error) {
+ros.on('error', function (error) {
   console.log('Error connecting to websocket server: ', error);
 });
 
-ros.on('close', function() {
+ros.on('close', function () {
   console.log('Connection to websocket server closed.');
 });
 
@@ -90,30 +121,33 @@ var robotPositionListener = new ROSLIB.Topic({
 setTimeout(() => {
   console.log('Wait ends after 3 seconds');
   // Subscribing to the ROS topic
-  robotPositionListener.subscribe(function(message) {
-  updateDronePositions(message.positions); // Directly update positions
+  robotPositionListener.subscribe(function (message) {
+    updateDronePositions(message.positions); // Directly update positions
   });
-  
-}, 3000); 
+
+}, 3000);
 
 
 
-// Modified animate function to simply render the scene
 function animate() {
   requestAnimationFrame(animate);
   controls.update();
   renderer.render(scene, camera);
 }
+
 animate()
 
 
-
-
-window.addEventListener('resize',()=>{
-  camera.aspect = window.innerWidth / window.innerHeight;
+window.addEventListener('resize', () => {
+  const newAspectRatio = window.innerWidth / window.innerHeight;
+  const newFrustumWidth = frustumHeight * newAspectRatio;
+  camera.left = -newFrustumWidth / 2;
+  camera.right = newFrustumWidth / 2;
+  camera.top = frustumHeight / 2;
+  camera.bottom = -frustumHeight / 2;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
-})
+});
 
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -121,26 +155,39 @@ document.addEventListener('DOMContentLoaded', function () {
   const spreadButton = document.getElementById('spreadButton');
   const stopButton = document.getElementById('stopButton');
 
-  spreadButton.addEventListener('click', function () {
-    fetch('http://localhost:3000/spread')
-    .then(response => response.text())
-    .then(data => console.log(data))
-    .catch(error => console.error('Error:', error));
-  });
+  const isConnected = false;
+  const introScreen = document.querySelector('.demo-intro')
+  const loadingScreen = document.querySelector('.demo-loading')
+  const activeScreen = document.querySelector('.demo-active')
+
 
   connectButton.addEventListener('click', function () {
+    introScreen.classList.remove('visible')
+    activeScreen.classList.add('visible')
+
     fetch('http://localhost:3000/start-robots')
-        .then(response => response.text())
-        .then(data => console.log(data))
-        .catch(error => console.error('Error:', error));
-    document.body.style.backgroundColor = 'red';
-});
+      .then(response => response.text())
+      .then(data => console.log(data))
+      .catch(error => console.error('Error:', error));
+  });
+
+  spreadButton.addEventListener('click', function () {
+    fetch('http://localhost:3000/spread')
+      .then(response => response.text())
+      .then(data => console.log(data))
+      .catch(error => console.error('Error:', error));
+  });
+
   stopButton.addEventListener('click', function () {
+    activeScreen.classList.remove('visible')
+    introScreen.classList.add('visible')
     fetch('http://localhost:3000/stop-robots')
-        .then(response => response.text())
-        .then(data => console.log(data))
-        .catch(error => console.error('Error:', error));
-});
+      .then(response => response.text())
+      .then(data => console.log(data))
+      .catch(error => console.error('Error:', error));
+  });
+
+
 });
 
 
